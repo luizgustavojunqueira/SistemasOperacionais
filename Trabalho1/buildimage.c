45bf549cc29fb931d06ad62c94b5fe1eb7353114
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2024-03-30 18:36:47 -0400

    codigo inicial correto

--- Trabalho1/buildimage.c
+++ Trabalho1/buildimage.c
@@ -1,6 +1,6 @@
 /* Author(s): <Your name(s) here>
  * Creates operating system image suitable for placement on a boot disk
- */
+*/
 /* TODO: Comment on the status of your submission. Largely unimplemented */
 #include <assert.h>
 #include <elf.h>
@@ -13,36 +13,37 @@
 #define IMAGE_FILE "./image"
 #define ARGS "[--extended] <bootblock> <executable-file> ..."
 
-#define SECTOR_SIZE 512				/* floppy sector size in bytes */
+#define SECTOR_SIZE 512       /* floppy sector size in bytes */
 #define BOOTLOADER_SIG_OFFSET 0x1fe /* offset for boot loader signature */
 // more defines...
 
 /* Reads in an executable file in ELF format*/
-Elf32_Phdr *read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr)
-{
+Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr)
+{ 
 	return NULL;
 }
 
 /* Writes the bootblock to the image file */
-void write_bootblock(FILE **imagefile, FILE *bootfile, Elf32_Ehdr *boot_header, Elf32_Phdr *boot_phdr)
+void write_bootblock(FILE **imagefile,FILE *bootfile,Elf32_Ehdr *boot_header, Elf32_Phdr *boot_phdr)
 {
 }
 
 /* Writes the kernel to the image file */
-void write_kernel(FILE **imagefile, FILE *kernelfile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr)
-{
+void write_kernel(FILE **imagefile,FILE *kernelfile,Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr)
+{ 
 }
 
 /* Counts the number of sectors in the kernel */
 int count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr)
-{
-	return 0;
+{   
+    return 0;
 }
 
 /* Records the number of sectors in the kernel */
-void record_kernel_sectors(FILE **imagefile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int num_sec)
-{
+void record_kernel_sectors(FILE **imagefile,Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int num_sec)
+{    
 }
+
 
 /* Prints segment information for --extended option */
 void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec)
@@ -50,9 +51,12 @@ void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec)
 
 	/* print number of disk sectors used by the image */
 
+  
 	/* bootblock segment info */
+ 
 
 	/* print kernel segment info */
+  
 
 	/* print kernel size in sectors */
 }
@@ -61,18 +65,18 @@ void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec)
 /* MAIN */
 int main(int argc, char **argv)
 {
-	FILE *kernelfile, *bootfile, *imagefile;				// file pointers for bootblock,kernel and image
-	Elf32_Ehdr *boot_header = malloc(sizeof(Elf32_Ehdr));	// bootblock ELF header
+	FILE *kernelfile, *bootfile,*imagefile;  // file pointers for bootblock,kernel and image
+	Elf32_Ehdr *boot_header   = malloc(sizeof(Elf32_Ehdr)); // bootblock ELF header
 	Elf32_Ehdr *kernel_header = malloc(sizeof(Elf32_Ehdr)); // kernel ELF header
-
+	
 	Elf32_Phdr *boot_program_header;   // bootblock ELF program header
 	Elf32_Phdr *kernel_program_header; // kernel ELF program header
 
 	/* build image file */
 
-	/* read executable bootblock file */
+	/* read executable bootblock file */  
 
-	/* write bootblock */
+	/* write bootblock */  
 
 	/* read executable kernel file */
 
@@ -81,10 +85,12 @@ int main(int argc, char **argv)
 	/* tell the bootloader how many sectors to read to load the kernel */
 
 	/* check for  --extended option */
-	if (!strncmp(argv[1], "--extended", 11))
-	{
+	if(!strncmp(argv[1], "--extended", 11)) {
 		/* print info */
 	}
-
+  
 	return 0;
 } // ends main()
+
+
+
--- Trabalho1/kernel.s
+++ Trabalho1/kernel.s
@@ -1,132 +1,121 @@
-.data                               # Data segment
-  
-# Some strings 
-kernel:
-  .asciz  "Kernel > "
-testing:
-  .asciz  "Running a trivial test... "
-works:
-  .asciz  "Test passed!"
-not:
-  .asciz  "*Failed*"
-
-# 'Newline' string ('carriage return', 'linefeed', '\0')
-newline:
-  .byte 10
-  .byte 13
-  .byte 0
-
-# An integer
-result:
-  .word 1000
-
-
-  
-.text                              # Code segment
-.code16                             # Real mode
+.text	                            # Code segment
+.code16                           
 .globl _start                       # The entry point must be global
-  
+
+  .equ START, 0x1000
+  .equ BOOT_SECTOR_SIZE, 0x200
+  .equ PARAGRAPH, 0x10
+
+  .equ TESTDATA_SEGMENT, 0x14c0
+  .equ TESTDATA_OFFSET, 0
 #
 # The first instruction to execute in a program is called the entry
 # point. The linker expects to find the entry point in the "symbol" _start
 # (with underscore).
 #
 _start:
-  pushw  %bp		# Setup stack frame
-  movw  %sp,%bp
-  
-  pushw  $newline
-  call  displayString	# Print messages
-  pushw  $kernel
-  call  displayString
-  pushw  $testing
-  call  displayString
-  pushw  $1000
-  call  trivialTest	# trivialTest(1000)
-  addw  $8,%sp		# Pop newline, kernel, testing, and '1000'
-  cmpw  %ax,result		
-  jne  .L6		# If (trivialTest(1000) != 1000) goto L6
-  pushw  $works			
-  jmp  .L12			
-.L6:    		# Test failed
-  pushw  $not			
-.L12:
-  call  displayString	# Print ok/failed message
-  addw  $2,%sp
-  pushw  $newline
-  call  displayString
-  addw    $2,%sp
-.L8:    		# Loop forever
-  jmp .L8
+  mov $okay1,%si
+  call print_string
 
-#
-# int trivialTest(n)
-# {
-#     if (n > 0) {
-#         trivialTest(n-1);
-#     }
-#     return n; 
-# }
-  
-trivialTest:  
-  pushw  %bp		# Setup stack frame
-  movw  %sp,%bp
-  movw  4(%bp),%ax	# Move argument to ax
-  testw  %ax,%ax		# Logical compare (sets SF, ZF and PF)
-  jg  .L2		# if (argument > 0) goto L2
-  xorw  %ax,%ax		# else return 0
-  popw  %bp			
-  retw  			
-.L2:
-  decw  %ax
-  pushw  %ax
-  call  trivialTest	# trivialTest(argument - 1)
-    		# (Recursive calls until argument == 0)
-  addw  $2,%sp		# Pop argument
-  incw  %ax
-  popw  %bp
-  retw  		# Return (argument in ax)
-  
-displayString:
-  pushw  %bp		# Setup stack frame
-  movw  %sp,%bp
-  pushw  %ax		# Save ax, bx, cx, si, es
-  pushw  %bx
-  pushw  %cx
-  pushw  %si
-  pushw  %es
-  movw  %ds, %ax	# Make sure ES points to the right
-  movw  %ax, %es	#  segment
-  movw  4(%bp),%cx	# Move string adr to cx
-  movw    %cx, %si
-loop:    
-  lodsb  		# Load character to write (c) into al,
-    		#  and increment si
-  cmpb  $0, %al		
-  jz  done		# if (c == '\0') exit loop
-  movb  $14,%ah		# else print c
-  movw  $0x0002,%bx
-  # int 0x10 sends a character to the display
-  # ah = 0xe (14)
-  # al = character to write
-  # bh = active page number (we use 0x00)
-  # bl = foreground color (we use 0x02)
-  int  $0x10           
-  jmp  loop
-done:
-  popw  %es		# Restore saved registers
-  popw  %si
-  popw  %cx
-  popw  %bx
-  popw  %ax
-  popw  %bp
-  retw  		# Return to caller
+  mov $TESTDATA_SEGMENT,%bx
+  mov %bx, %es
+  movb %es:(0), %al
+  cmp $'T',%al
+  jnz fail
+
+  mov $okay2,%si
+  call print_string
+
+  movb %es:(1), %al
+  cmp $'h',%al
+  jnz fail
+
+  mov $0x0301, %ax
+  mov $0x0001, %cx
+  mov $TESTDATA_OFFSET, %bx
+  mov $0x0001, %dx
+  int $0x13
+
+  mov $okay3,%si
+  call print_string
+
+shutdown:
+  # shut down the computer
+  movw $0x5307, %ax
+  movw $0x01, %bx
+  movw $0x03, %cx
+#  int $0x15
+
+  # in case that didn't work
+halt:
+  hlt
+  jmp halt
+
+fail:
+  mov $bad, %si
+  call print_string
+  jmp shutdown
+
+okay1: .asciz "Passed test 1\n\r"
+okay2: .asciz "Passed test 2\n\r"
+okay3: .asciz "Passed test 3\n\r"
+bad: .asciz "Bad!\n\r"
+
+print_string:
+  lodsb
+  cmp $0, %al
+  jz print_string_done
+  call print_char
+  jmp print_string
+print_string_done:
+  ret
+
+print_char:
+  movb $0xe, %ah
+  movw $0x0002, %bx
+  int $0x10
+  ret
 
 
+  .org 150 * BOOT_SECTOR_SIZE + START
 
+testdata:
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
 
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
 
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
 
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+  .string "This is sixteen "
+end:
 
 
 
