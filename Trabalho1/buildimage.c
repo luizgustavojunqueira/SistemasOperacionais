99f6f22675ffb3c629e07e1d4e132d486f4ed2fb
Luiz Gustavo Junqueira <luizgustavossj@gmail.com>
2024-04-01 20:12:53 -0400

    leitura de arquivos executaveis e escrita do bootblock

--- Trabalho1/buildimage.c
+++ Trabalho1/buildimage.c
@@ -1,4 +1,4 @@
-/* Author(s): <Your name(s) here>
+/* Author(s): Luiz Gustavo S. S. Junqueira, Camila Cardoso Cáceres
  * Creates operating system image suitable for placement on a boot disk
  */
 /* TODO: Comment on the status of your submission. Largely unimplemented */
@@ -21,31 +21,28 @@
 Elf32_Phdr *read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr)
 {
 	Elf32_Phdr *phdr = malloc(sizeof(Elf32_Phdr));
-	Elf32_Ehdr *header = malloc(sizeof(Elf32_Ehdr));
 
-	/* open file */
 	*execfile = fopen(filename, "r");
 	if (*execfile == NULL)
 	{
-		fprintf(stderr, "Error: Unable to open file %s\n", filename);
-		exit(1);
+		printf("Erro: Não foi possível abrir o arquivo %s\n", filename);
+		return NULL;
 	}
 
-	/* read ELF header */
-	fread(header, sizeof(Elf32_Ehdr), 1, *execfile);
+	// Lê o cabeçalho ELF do arquivo
+	fread(*ehdr, sizeof(Elf32_Ehdr), 1, *execfile);
 
-	/* check ELF header */
-	if (header->e_ident[0] != 0x7f || header->e_ident[1] != 'E' || header->e_ident[2] != 'L' || header->e_ident[3] != 'F')
+	// Verifica se o arquivo é um arquivo ELF
+	if ((*ehdr)->e_ident[0] != 0x7f || (*ehdr)->e_ident[1] != 'E' || (*ehdr)->e_ident[2] != 'L' || (*ehdr)->e_ident[3] != 'F')
 	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		exit(1);
+		printf("Erro: Não é um arquivo ELF\n");
+		return NULL;
 	}
 
-	/* read program header */
-	fseek(*execfile, header->e_phoff, SEEK_SET);
+	// Move o ponteiro de escrita para o início do cabeçalho do programa
+	fseek(*execfile, (*ehdr)->e_phoff, SEEK_SET);
+	// Lê o cabeçalho do programa
 	fread(phdr, sizeof(Elf32_Phdr), 1, *execfile);
-
-	*ehdr = header;
 
 	return phdr;
 }
@@ -53,73 +50,34 @@ Elf32_Phdr *read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr)
 /* Writes the bootblock to the image file */
 void write_bootblock(FILE **imagefile, FILE *bootfile, Elf32_Ehdr *boot_header, Elf32_Phdr *boot_phdr)
 {
-	// Calculate the number of sectors needed to store the bootblock
+
 	int num_sec = (boot_phdr->p_filesz + SECTOR_SIZE - 1) / SECTOR_SIZE;
 
-	// Allocate a buffer to hold the bootblock data
-	char *buffer = malloc(num_sec * SECTOR_SIZE);
-	if (buffer == NULL)
-	{
-		fprintf(stderr, "Error: Unable to allocate memory\n");
-		exit(1);
-	}
+	/* write bootblock to image */
 
-	// Read the bootblock data from the bootfile
+	char *buffer = malloc(num_sec * SECTOR_SIZE);
 	fseek(bootfile, boot_phdr->p_offset, SEEK_SET);
 	fread(buffer, boot_phdr->p_filesz, 1, bootfile);
 
-	// Write the bootblock data to the image file
 	fwrite(buffer, num_sec * SECTOR_SIZE, 1, *imagefile);
 
-	// Free the buffer
 	free(buffer);
 }
 
 /* Writes the kernel to the image file */
 void write_kernel(FILE **imagefile, FILE *kernelfile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr)
 {
-	// Calculate the number of sectors needed to store the kernel
-	int num_sec = (kernel_phdr->p_filesz + SECTOR_SIZE - 1) / SECTOR_SIZE;
-
-	// Allocate a buffer to hold the kernel data
-	char *buffer = malloc(num_sec * SECTOR_SIZE);
-	if (buffer == NULL)
-	{
-		fprintf(stderr, "Error: Unable to allocate memory\n");
-		exit(1);
-	}
-
-	// Read the kernel data from the kernelfile
-	fseek(kernelfile, kernel_phdr->p_offset, SEEK_SET);
-	fread(buffer, kernel_phdr->p_filesz, 1, kernelfile);
-
-	// Write the kernel data to the image file
-	fwrite(buffer, num_sec * SECTOR_SIZE, 1, *imagefile);
-
-	// Free the buffer
-	free(buffer);
 }
 
 /* Counts the number of sectors in the kernel */
 int count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr)
 {
-	// Calculate the number of sectors needed to store the kernel
-	int num_sec = (kernel_phdr->p_filesz + SECTOR_SIZE - 1) / SECTOR_SIZE;
-
-	return num_sec;
+	return 0;
 }
 
 /* Records the number of sectors in the kernel */
 void record_kernel_sectors(FILE **imagefile, Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr, int num_sec)
 {
-	// Calculate the offset for recording the number of sectors
-	int offset = BOOTLOADER_SIG_OFFSET - sizeof(int);
-
-	// Seek to the offset in the image file
-	fseek(*imagefile, offset, SEEK_SET);
-
-	// Write the number of sectors to the image file
-	fwrite(&num_sec, sizeof(int), 1, *imagefile);
 }
 
 /* Prints segment information for --extended option */
@@ -127,32 +85,12 @@ void extended_opt(Elf32_Phdr *bph, int k_phnum, Elf32_Phdr *kph, int num_sec)
 {
 
 	/* print number of disk sectors used by the image */
-	printf("Number of disk sectors used by the image: %d\n", num_sec);
 
 	/* bootblock segment info */
-	printf("Bootblock segment info:\n");
-	printf("Type: %d\n", bph->p_type);
-	printf("Offset: %d\n", bph->p_offset);
-	printf("Vaddr: %d\n", bph->p_vaddr);
-	printf("Paddr: %d\n", bph->p_paddr);
-	printf("Filesz: %d\n", bph->p_filesz);
-	printf("Memsz: %d\n", bph->p_memsz);
-	printf("Flags: %d\n", bph->p_flags);
-	printf("Align: %d\n", bph->p_align);
 
 	/* print kernel segment info */
-	printf("Kernel segment info:\n");
-	printf("Type: %d\n", kph->p_type);
-	printf("Offset: %d\n", kph->p_offset);
-	printf("Vaddr: %d\n", kph->p_vaddr);
-	printf("Paddr: %d\n", kph->p_paddr);
-	printf("Filesz: %d\n", kph->p_filesz);
-	printf("Memsz: %d\n", kph->p_memsz);
-	printf("Flags: %d\n", kph->p_flags);
-	printf("Align: %d\n", kph->p_align);
 
 	/* print kernel size in sectors */
-	printf("Kernel size in sectors: %d\n", num_sec);
 }
 // more helper functions...
 
@@ -167,16 +105,17 @@ int main(int argc, char **argv)
 	Elf32_Phdr *kernel_program_header; // kernel ELF program header
 
 	/* build image file */
+
 	imagefile = fopen(IMAGE_FILE, "w");
 	if (imagefile == NULL)
 	{
-		fprintf(stderr, "Error: Unable to open file %s\n", IMAGE_FILE);
-		exit(1);
+		printf("Erro: Não foi possível criar o arquivo %s\n", IMAGE_FILE);
+		return 1;
 	}
 
-	boot_program_header = read_exec_file(&bootfile, argv[1], &boot_header);
+	/* read executable bootblock file */
 
-	// Print program header information
+	boot_program_header = read_exec_file(&bootfile, argv[1], &boot_header);
 
 	printf("Bootblock program header information:\n");
 	printf("Type: %d\n", boot_program_header->p_type);
@@ -188,9 +127,18 @@ int main(int argc, char **argv)
 	printf("Flags: %d\n", boot_program_header->p_flags);
 	printf("Align: %d\n", boot_program_header->p_align);
 
-	kernel_program_header = read_exec_file(&kernelfile, argv[2], &kernel_header);
+	/* write bootblock */
 
-	// Print program header information
+	write_bootblock(&imagefile, bootfile, boot_header, boot_program_header);
+
+	/*Write image file signature*/
+	fseek(imagefile, BOOTLOADER_SIG_OFFSET, SEEK_SET);
+	fwrite("\x55\xAA", 2, 1, imagefile);
+	
+
+	/* read executable kernel file */
+
+	kernel_program_header = read_exec_file(&kernelfile, argv[2], &kernel_header);
 
 	printf("Kernel program header information:\n");
 	printf("Type: %d\n", kernel_program_header->p_type);
@@ -202,21 +150,15 @@ int main(int argc, char **argv)
 	printf("Flags: %d\n", kernel_program_header->p_flags);
 	printf("Align: %d\n", kernel_program_header->p_align);
 
-	/* write bootblock */
-	write_bootblock(&imagefile, bootfile, boot_header, boot_program_header);
-
 	/* write kernel segments to image */
-	write_kernel(&imagefile, kernelfile, kernel_header, kernel_program_header);
 
 	/* tell the bootloader how many sectors to read to load the kernel */
-	int num_sec = count_kernel_sectors(kernel_header, kernel_program_header);
-	record_kernel_sectors(&imagefile, kernel_header, kernel_program_header, num_sec);
 
-	/* check for --extended option */
-	if (argc > 3 && !strncmp(argv[3], "--extended", 11))
+	/* check for  --extended option */
+	if (!strncmp(argv[1], "--extended", 11))
 	{
 		/* print info */
-		extended_opt(boot_program_header, argc - 3, kernel_program_header, num_sec);
+		extended_opt(boot_program_header, kernel_header->e_phnum, kernel_program_header, count_kernel_sectors(kernel_header, kernel_program_header));
 	}
 
 	return 0;
