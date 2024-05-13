da53775f2bf900b1eb6db398ab372817404b5d19
Camila Cardoso Cáceres <camilacacerescardoso@gmail.com>
2024-05-13 15:38:30 -0400

    implementacao da fila

--- Trabalho2/lib/queue.c
+++ Trabalho2/lib/queue.c
@@ -1,5 +1,7 @@
 #include <queue.h>
 #include <thread.h>
+#include <stdio.h>
+#include <stdlib.h>
 /*
   This   file    contains   a    function   definitions    for   queue
   manipulation. You  are free to  choose your own  implementation. You
@@ -8,42 +10,62 @@
  */
 // inicializes a queue with head
 node_t *first, *last;
-void queue_init(node_t * queue)
+void queue_init(node_t *queue)
 {
   queue = (node_t *)malloc(sizeof(node_t));
-  queue->next=NULL;
-  first=queue->next;
-  last=first;
-
+  queue->next = NULL;
+  first = queue->next;
+  last = first;
+  printf("init queue\n");
 }
 
 // TODO: returns the first element of the queue
-node_t *dequeue(node_t * queue)
+node_t *dequeue(node_t *queue)
 {
-  
-	return queue->next;
+  node_t *temp;
+  temp = queue->next;
+  queue->next = temp->next;
+  temp->next = NULL;
+
+  return temp;
 }
 
 // inserts a node in a queue
-void enqueue(node_t * queue, node_t * item)
+void enqueue(node_t *queue, node_t *item)
 {
-  node_t *newNode;
-  newNode = (node_t *) malloc(sizeof (node_t));
-  newNode->tcb_t = item;
-  newNode->next = queue->next;
-  queue->next = newNode;
+  node_t *p = queue->next;
+
+  if(p == NULL){
+    queue->next = item;
+    first = queue->next;
+    last = first;
+    return;
+  }
+
+  while(p->next != NULL){
+    p = p->next;
+  }
+  p->next = item;
+  last = p->next;
 
 }
 
 // TODO: checks if a queue is empty
 int is_empty(node_t *queue)
 {
-	if(queue->next==NULL)
+  if (queue->next == NULL)
   {
     return 1;
   }
   return 0;
 }
 
+void print_queue(node_t *queue){
+  node_t *p = queue->next;
+  while(p->next != NULL){
+    printf("%d\n", ((tcb_t *)p->thread->tcb)->tid);
+    p = p->next;
+  }
 
-
+  printf("%d\n", ((tcb_t *)p->thread->tcb)->tid);
+}
--- Trabalho2/lib/thread.c
+++ Trabalho2/lib/thread.c
@@ -6,8 +6,8 @@
 #include <queue.h>
 #include <thread.h>
 
-node_t	ready_queue;
-tcb_t	*current_running;
+node_t ready_queue;
+tcb_t *current_running;
 
 int tid_global = 0;
 
@@ -18,24 +18,52 @@ int tid_global = 0;
  */
 int thread_init()
 {
+	queue_init(&ready_queue);
 	return 0;
 }
 
 // TODO: creates a new thread and inserts in the ready queue.
 int thread_create(thread_t *thread, void *(*start_routine)(void *), void *arg)
 {
+
+	printf("%p", arg); 	
+
+	thread = (thread_t *)malloc(sizeof(thread_t));
+
+	tcb_t *tcb = (tcb_t *)malloc(sizeof(tcb_t));
+	tcb->arg = arg;
+	tcb->status = FIRST_TIME;
+	tcb->tid = tid_global++;
+	tcb->start_routine = start_routine;
+
+	thread->tcb = tcb;
+
+	node_t *newNode = (node_t *)malloc(sizeof(node_t));
+	newNode->thread = thread;
+
+	enqueue(&ready_queue, newNode);
+	
 	return 0;
 }
 
 // TODO: yields the CPU to another thread
 int thread_yield()
 {
+	node_t* temp = dequeue(&ready_queue);
+	enqueue(&ready_queue, temp);
+
+	printf("removido %d\n", ((tcb_t*) temp->thread->tcb)->tid);
+	
+	// printf("Na lista: \n");
+	// print_queue(&ready_queue);
 	return 0;
 }
 
 // TODO: waits for a thread to finish
-int thread_join(thread_t *thread, int *retval) 
+int thread_join(thread_t *thread, int *retval)
 {
+	tcb_t *tcb = (tcb_t*) thread->tcb;
+	tcb->start_routine(tcb->arg);
 	return 0;
 }
 
@@ -54,4 +82,3 @@ void scheduler()
 void exit_handler()
 {
 }
-
